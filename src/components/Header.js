5d5bd2c7198ca7712bb8329f8f6afe0cfe0c43b6
Rishav Sah <rishavsah4u@gmail.com>
2023-08-28 23:40:31 +0545

    tailwind css and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,42 +2,64 @@ import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-
+  const [nav, setNav] = useState(false);
   const [btnName, setBtnName] = useState("Login");
   const onlineStatus = useOnlineStatus();
+
+  const handleNav = () => {
+    console.log(nav)
+    setNav(!nav);
+  };
 
   console.log("Header Rendered");
   
   return (
-    <header className="header">
-      <div className="logo-container">
-        <img className="logo" src={LOGO_URL} alt="app-logo" />
+    <header className="p-4 w-full md:flex justify-between items-center text-gray-800">
+      <div className="w-20">
+        <img className="w-full" src={LOGO_URL} alt="app-logo" />
       </div>
-      <nav className="nav-items">
-        <ul>
-          <li>
-            Online Status:{onlineStatus ? "✅" : "🔴"}
+      
+      <nav>
+        <div className="absolute right-8 top-6 cursor-pointer md:hidden">
+          <button onClick={handleNav} className="cursor-pointer">
+            {!nav ? <FontAwesomeIcon icon={faBars} size="lg" /> : <FontAwesomeIcon icon={faClose} size="lg" />}
+          </button>
+        </div>
+        <ul className={`py-4 absolute left-0 w-full bg-gray-200 transition-opacity ease-in duration-300 md:static md:flex md:items-center md:bg-white md:opacity-100 ${nav ? "opacity-100" : "opacity-0"}`}>
+          <li className="mx-2 rounded-md px-4 py-2 font-medium transition-colors duration-300 cursor-pointer hover:bg-gray-400">
+            {onlineStatus ? "✅" : "🔴"}
           </li>
-          <li>
-            <Link to="/">Home</Link>
+          <li className="mx-2 rounded-md px-4 py-2 font-medium transition-colors duration-300 cursor-pointer hover:bg-gray-400">
+            <Link to="/" onClick={handleNav}>Home</Link>
           </li>
-          <li>
-            <Link to="about">About</Link>
+          <li className="mx-2 rounded-md px-4 py-2 font-medium transition-colors duration-300 cursor-pointer hover:bg-gray-400">
+            <Link to="about" onClick={handleNav}>About</Link>
           </li>
-          <li>
-            <Link to="contact">Contact</Link>
+          <li className="mx-2 rounded-md px-4 py-2 font-medium transition-colors duration-300 cursor-pointer hover:bg-gray-400">
+            <Link to="contact" onClick={handleNav}>Contact</Link>
           </li>
-          <li>
-            <Link to="grocery">Grocery</Link>
-          </li>
-          <li>Cart</li>
-          <li>
-            <button className="login-btn" onClick={() => btnName == "Login" ? setBtnName("Logout") : setBtnName("Login")}>{btnName}</button>
+          <li className="mx-2 rounded-md px-4 py-2 font-medium transition-colors duration-300 cursor-pointer hover:bg-gray-400">
+            <Link to="grocery" onClick={handleNav}>Grocery</Link>
           </li>
         </ul>
       </nav>
+      <div className="absolute right-14 top-4 md:static">
+        <button
+            className="mx-4 rounded-md px-4 py-2 font-medium bg-gray-400 transition-colors duration-300 cursor-pointer"
+          >
+            Cart
+          </button>
+        <button
+          className="mx-4 rounded-md px-4 py-2 font-medium bg-gray-400 transition-colors duration-300 cursor-pointer"
+          onClick={() => btnName == "Login" ? setBtnName("Logout") : setBtnName("Login")}
+        >
+          {btnName}
+        </button>
+      </div>
     </header>
   );
 };
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,15 +3,15 @@ import { CDN_URL } from "../utils/constant";
 const RestaurantCard = (props) => {
   const {cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla} = props?.resData?.info;
   return (
-    <div className="res-card">
-      <div className="res-logo">
-        <img src={`${CDN_URL}/${cloudinaryImageId}`} alt="res-image" />
+    <div className="rounded-lg p-4 w-auto shadow-md">
+      <div className="mb-6 w-50 h-40 rounded-md overflow-hidden">
+        <img className="w-full h-full object-cover" src={`${CDN_URL}/${cloudinaryImageId}`} alt="res-image" />
       </div>
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating}</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{sla?.deliveryTime}</h4>
+      <h3 className="my-2 text-xl font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis">{name}</h3>
+      <h4 className="my-2 text-sm whitespace-nowrap overflow-hidden overflow-ellipsis">{cuisines.join(", ")}</h4>
+      <h4 className="my-2 text-sm">{avgRating}</h4>
+      <h4 className="my-2 text-sm">{costForTwo}</h4>
+      <h4 className="my-2 text-sm">{sla?.deliveryTime}</h4>
     </div>
   );
 };
